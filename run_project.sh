9a9ae7f620d707382098f613fb2fcc605e769a4e
sam505 <mainasamuel505@gmail.com>
2020-10-13 17:01:59 +0300

    'Making it better'

--- run_project.sh
+++ run_project.sh
@@ -1,2 +1,2 @@
-python src/main.py --input_type cam --input_file /home/sammie/Downloads/Computer_pointer_controller/bin/demo.mp4 --show_results yes --model_path models/intel/
+python src/main.py --input_type cam --input_file  --show_results yes --model_path models/intel/
 
