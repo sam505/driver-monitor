28b780a6a75cac2c0d4d48259f52a5f7aebb3264
sam505 <mainasamuel505@gmail.com>
2020-10-13 16:33:28 +0300

    'Making it better'

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from src.face_detection import FaceDetection
 from src.facial_landmarks_detection import FacialLandmarks
 from src.head_pose_estimation import HeadPoseEstimation
 from src.gaze_estimation import GazeEstimation
+from playsound import playsound
 import logging
 
 logger = logging.getLogger(__name__)
@@ -10,6 +11,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def main():
+    audio = "bin/beep-01a.mp3"
     fd = FaceDetection(model_name='models/intel/face-detection-adas-binary-0001/FP32-INT1/face-detection-adas-'
                                   'binary-0001')
     fl = FacialLandmarks(model_name='models/intel/landmarks-regression-retail-0009/FP32/landmarks-regression-'
@@ -25,8 +27,8 @@ def main():
     count = 0
     total_score = 0
 
-    # cap = cv2.VideoCapture('/home/sammie/Videos/demo.mp4')
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture('/home/sammie/PycharmProjects/driver\'s_attention/bin/Guardian_driver1.mp4')
+    # cap = cv2.VideoCapture(0)
     while cap.isOpened():
         _, frame = cap.read()
         count += 1
@@ -79,6 +81,7 @@ def main():
 
             total_score_value = total_score/(count * 5)
             if total_score_value < 0.9:
+                playsound(audio)
                 color = (0, 0, 255)
             else:
                 color = (0, 255, 0)
@@ -99,6 +102,7 @@ def main():
             total_score_value = total_score / (count * 5)
             image = cv2.resize(frame, (1280, 700), interpolation=cv2.INTER_AREA)
             if total_score_value < 0.9:
+                playsound(audio)
                 color = (0, 0, 255)
             else:
                 color = (0, 255, 0)
